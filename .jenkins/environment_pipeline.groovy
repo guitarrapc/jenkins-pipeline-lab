5eded5cf475ab5de2601255ed8f7e82a03eadfd1
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-28 16:56:30 +0900

    chore: environment override on stage

--- .jenkins/environment_pipeline.groovy
+++ .jenkins/environment_pipeline.groovy
@@ -3,12 +3,23 @@ pipeline {
     environment {
         FOO = "foo"
         BAR = "bar"
+        BASE = "pipeline"
     }
     stages {
         stage('hello') {
             steps {
                 echo "FOO: ${FOO}"
                 echo "BAR: ${BAR}"
+                echo "BASE: ${BASE}"
+            }
+        }
+
+        stage('STAGE OVERRIDE') {
+            environment {
+                BASE = "stage"
+            }
+            steps {
+                echo "BASE: ${BASE}"
             }
         }
     }
