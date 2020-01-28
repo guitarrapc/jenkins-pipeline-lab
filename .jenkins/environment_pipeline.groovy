48f02877b99699d1ed45a28288e8d29277e46d2e
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-28 17:45:47 +0900

    chore: environment to see system env

--- .jenkins/environment_pipeline.groovy
+++ .jenkins/environment_pipeline.groovy
@@ -6,6 +6,22 @@ pipeline {
         BASE = "pipeline"
     }
     stages {
+        stage('spin up') {
+            steps {
+                sh "export"
+            }
+        }
+        stage('system env') {
+            steps {
+                echo "BUILD_ID: ${BUILD_ID}"
+                echo "BUILD_TAG: ${BUILD_TAG}"
+                echo "BUILD_URL: ${BUILD_URL}"
+                echo "GIT_BRANCH: ${GIT_BRANCH}"
+                echo "GIT_COMMIT: ${GIT_COMMIT}"
+                echo "GIT_URL: ${GIT_URL}"
+                echo "JOB_BASE_NAME: ${JOB_BASE_NAME}"
+            }
+        }
         stage('hello') {
             steps {
                 echo "FOO: ${FOO}"
