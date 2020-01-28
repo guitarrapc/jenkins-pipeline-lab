2864be6e7b066e02c1e524ff73f7e12be1948405
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-28 16:15:23 +0900

    chore: parallel stage

---
+++ .jenkins/parallel_pipeline.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    stages {
+        stage('hello') {
+          parallel{
+            stage('hello1') {
+              steps {
+                  echo "hello1"
+              }
+            }
+            stage('hello2'){
+              steps {
+                  echo "hello2"
+              }
+            }
+          }
+        }
+    }
+}
