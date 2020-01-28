db7b92e66ec88ac946a6c23990ba7b177c2ccf4b
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-28 17:52:04 +0900

    options

---
+++ .jenkins/options_pipeline copy.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+
+    // https://jenkins.io/doc/book/pipeline/syntax/#options
+    options {
+        // General Jenkins job properties
+        buildDiscarder(logRotator(numToKeepStr:'1'))
+        // Declarative-specific options
+        skipDefaultCheckout()
+        // "wrapper" steps that should wrap the entire build execution
+        timestamps()
+        timeout(time: 5, unit: 'MINUTES')
+        // retry pipeline when failed
+        retry(3)
+    }
+
+    stages {
+        stage('hello') {
+            options {
+                timeout(time: 1, unit: 'HOURS')
+            }
+            steps {
+                echo 'hello world'
+            }
+        }
+    }
+}
