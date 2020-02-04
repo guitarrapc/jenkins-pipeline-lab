8da91630fda67ae4c1de16daaf3451dcdca47f53
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-02-04 10:35:03 +0900

    chore: nest

--- pipelines/nested-stage_pipeline.groovy
+++ pipelines/nested-stage_pipeline.groovy
@@ -1,15 +1,17 @@
 pipeline {
   agent any
-  stage('nest') {
-    stages {
-      stage('hello1') {
-        steps {
-          echo "hello1"
+  stages{
+    stage('nest') {
+      stages {
+        stage('hello1') {
+          steps {
+            echo "hello1"
+          }
         }
-      }
-      stage('hello2'){
-        steps {
-            echo "hello2"
+        stage('hello2'){
+          steps {
+              echo "hello2"
+          }
         }
       }
     }
