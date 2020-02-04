ae2cb5de5b9d57728cc84b43e21744ec1082cc10
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-02-04 10:42:01 +0900

    chore: fix lint

--- pipelines/parallel_pipeline.groovy
+++ pipelines/parallel_pipeline.groovy
@@ -3,32 +3,28 @@ pipeline {
   stages{
     stage('nest1') {
       parallel {
-        stages {
-          stage('hello1-1') {
-            steps {
-              echo "hello1"
-            }
+        stage('hello1-1') {
+          steps {
+            echo "hello1"
           }
-          stage('hello1-2'){
-            steps {
-                echo "hello2"
-            }
+        }
+        stage('hello1-2'){
+          steps {
+              echo "hello2"
           }
         }
       }
     }
     stage('nest2') {
       parallel{
-        stages {
-          stage('hello2-1') {
-            steps {
-              echo "hello1"
-            }
+        stage('hello2-1') {
+          steps {
+            echo "hello1"
           }
-          stage('hello2-2'){
-            steps {
-                echo "hello2"
-            }
+        }
+        stage('hello2-2'){
+          steps {
+              echo "hello2"
           }
         }
       }
