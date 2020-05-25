ac42bcfafce5bcb2254c7e375749ccc2e3344d0f
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-05-25 15:10:27 +0900

    chore: which status will be hit

--- pipelines/catcherror_pipeline.groovy
+++ pipelines/catcherror_pipeline.groovy
@@ -14,7 +14,21 @@ pipeline {
       }
       post {
         always {
-          echo "post failure: error happen"
+          echo "post always: inside stage"
+        }
+        // changed means when the build status is different than the previous build's status.
+        changed {
+          echo "post changed: inside stage"
+        }
+        // success, failure, unstable all run if the current build status is successful, failed, or unstable, respectively
+        success {
+          echo "post success: inside stage"
+        }
+        failure {
+          echo "post failure: inside stage"
+        }
+        unstable {
+          echo "post unstable: inside stage"
         }
       }
     }
