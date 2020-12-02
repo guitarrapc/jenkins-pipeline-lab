44f63258e594d4e59e4c884112be4a2a44d8f1b5
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-12-03 03:37:56 +0900

    feat: add dotnet build pipeline

---
+++ pipelines/dotnet_build_pipeline.groovy
@@ -0,0 +1,45 @@
+pipeline {
+  agent {
+    docker "mcr.microsoft.com/dotnet/sdk:5.0"
+  }
+  parameters {
+    string(name: 'BRANCH', defaultValue: 'master', description: 'GitHub branch to build.')
+  }
+  options {
+    skipDefaultCheckout()
+    timestamps()
+  }
+  stages {
+    stage('checkout') {
+      steps {
+        checkout(
+          changelog: true,
+          poll: true, /*This is the important option to poll this repo change*/
+          scm: [
+              $class: 'GitSCM',
+              branches: [[name: "*/$env.BRANCH"]], // leave blank to check all branches
+              doGenerateSubmoduleConfigurations: false,
+              extensions: [
+                [$class: 'CleanCheckout'], // clean before checkout
+                [$class: 'RelativeTargetDirectory', relativeTargetDir: "src/"], // checkout only when "src/"" folder has changes.
+                [$class: 'CloneOption', depth: 1, noTags: false, reference: '', shallow: true] // shallow clone with depth 1
+              ],
+              submoduleCfg: [],
+              userRemoteConfigs: [[url: 'https://github.com/guitarrapc/SkiaSharp.QrCode.git']]
+            ]
+        )
+      }
+    }
+    stage('build') {
+      steps {
+        sh "dotnet --version"
+        dir("src/") {
+          sh """
+            pwd
+            ls -l
+          """
+        }
+      }
+    }
+  }
+}
